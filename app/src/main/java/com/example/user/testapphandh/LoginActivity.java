4a08551afecdf5f7ec1834f1ab66253518fb1d13
Alex Novikov <luck.alex13@gmail.com>
2019-03-03 17:34:51 +0800

    Маню в тулбаре

--- app/src/main/java/com/example/user/testapphandh/LoginActivity.java
+++ app/src/main/java/com/example/user/testapphandh/LoginActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,5 +14,13 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.login_toolbar_menu, menu);
+        return true;
     }
 }
