9ce14bb4c684417e70314425806e06ce5aa21879
Alex Novikov <luck.alex13@gmail.com>
2019-03-03 18:48:24 +0800

    Добавил биндинги в верстку и заюзал в активити

--- app/src/main/java/com/example/user/testapphandh/LoginActivity.java
+++ app/src/main/java/com/example/user/testapphandh/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.testapphandh;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -7,17 +8,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.user.testapphandh.databinding.ActivityLoginBinding;
+
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        setUpToolbar();
+        ActivityLoginBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        setUpToolbar(binding.toolbar);
     }
 
-    private void setUpToolbar(){
-        Toolbar toolbar = findViewById(R.id.toolbar);
+    private void setUpToolbar(Toolbar toolbar){
         toolbar.setTitle(R.string.title_activity_login);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
