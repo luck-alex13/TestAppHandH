686bb85b4f932189c9a1cd48d4093d3e52ebe737
Alex Novikov <luck.alex13@gmail.com>
2019-03-03 17:00:50 +0800

    Разделил верстку тулбара и контента

--- app/src/main/java/com/example/user/testapphandh/LoginActivity.java
+++ app/src/main/java/com/example/user/testapphandh/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.user.testapphandh;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
