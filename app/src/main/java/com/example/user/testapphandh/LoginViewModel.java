7e2202c88950b8729d165fd1346761587f3973ae
Alex Novikov <luck.alex13@gmail.com>
2019-03-07 00:07:49 +0800

    Добавил обработку ошибок

--- app/src/main/java/com/example/user/testapphandh/LoginViewModel.java
+++ app/src/main/java/com/example/user/testapphandh/LoginViewModel.java
@@ -9,19 +9,23 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
-import com.example.user.testapphandh.data.WeatherResponse;
 import com.example.user.testapphandh.helpers.SnackbarHelper;
 import com.example.user.testapphandh.helpers.Utils;
 import com.example.user.testapphandh.helpers.ValidationHelper;
 import com.example.user.testapphandh.network.Const;
 import com.example.user.testapphandh.network.WeatherClient;
 
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 import java.util.Locale;
 
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
+import retrofit2.HttpException;
 
 public class LoginViewModel extends AndroidViewModel {
+
+    private static final String TAG = LoginViewModel.class.getSimpleName();
 
     public LoginViewModel(@NonNull Application application) {
         super(application);
@@ -70,17 +74,14 @@ public class LoginViewModel extends AndroidViewModel {
         if (checkConnection(getApplication(), R.string.no_internet_connection)) {
             Disposable dp = WeatherClient.getInstance()
                     .getWeatherInCity(Const.SPB_CITY_ID, Locale.getDefault().getLanguage(), Const.UNIT_METRIC)
-                    .subscribe(new Consumer<WeatherResponse>() {
-                        @Override
-                        public void accept(WeatherResponse weatherResponse) throws Exception {
-                            String weather = Utils.format(view.getContext(), R.string.weather_info,
-                                    weatherResponse.getMain().getTemp(),
-                                    weatherResponse.getMain().getHumidity(),
-                                    weatherResponse.getWind().getSpeed());
+                    .subscribe(weatherResponse -> {
+                        String weather = Utils.format(view.getContext(), R.string.weather_info,
+                                weatherResponse.getMain().getTemp(),
+                                weatherResponse.getMain().getHumidity(),
+                                weatherResponse.getWind().getSpeed());
 
-                            SnackbarHelper.showSnack(view, weather);
-                        }
-                    });
+                        SnackbarHelper.showSnack(view, weather);
+                    }, throwable -> handleError(getApplication(), throwable));
         }
     }
 
@@ -90,6 +91,21 @@ public class LoginViewModel extends AndroidViewModel {
             Utils.showToast(appContext, errStr);
         }
         return res;
+    }
+
+    private void handleError(Context context, Throwable throwable) {
+        //throwable.printStackTrace();
+        Utils.err(TAG, throwable.getMessage(), throwable);
+        if (throwable instanceof HttpException) {
+            HttpException e = (HttpException) throwable;
+            Utils.showToast(context, Utils.format(context, R.string.network_error_code, e.code()));
+        } else if (throwable instanceof SocketTimeoutException) {
+            Utils.showToast(context, R.string.server_timeout);
+        } else if (throwable instanceof ConnectException || throwable instanceof UnknownHostException) {
+            Utils.showToast(context, R.string.no_internet_connection);
+        } else {
+            Utils.showToast(context, R.string.unhandled_error);
+        }
     }
 
 }
